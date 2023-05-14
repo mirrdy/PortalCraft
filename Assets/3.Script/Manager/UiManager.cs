b2e2f0d511905694e40220d60abf6caa503567c8
gure123 <83895630+gure123@users.noreply.github.com>
2023-05-14 18:55:07 +0900

    setting

--- Assets/3.Script/Manager/UiManager.cs
+++ Assets/3.Script/Manager/UiManager.cs
@@ -5,14 +5,15 @@ using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour
 {
+    [Header("Windows")]
+    [SerializeField] GameObject settingWindow;
+
     [Header("Setting Window")]
     [SerializeField] Slider BGM_Slider;
     [SerializeField] Slider SFX_Slider;
 
-    private void Awake()
-    {
-        
-    }
+    float bgmValue = 1f;
+    float sfxValue = 1f;
 
     public void BGM_VolumeSetting()
     {
@@ -25,5 +26,18 @@ public class UiManager : MonoBehaviour
         {
             AudioManager.instance.SFX_Play[i].volume = SFX_Slider.value;
         }
+    }
+
+    public void SaveButton()
+    {
+        bgmValue = BGM_Slider.value;
+        sfxValue = SFX_Slider.value;
+    }
+
+    public void BackButton()
+    {
+        BGM_Slider.value = bgmValue;
+        SFX_Slider.value = sfxValue;
+        settingWindow.SetActive(false);
     }
 }
