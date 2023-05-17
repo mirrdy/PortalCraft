28a4fd0071abceb2cca9abe0ad65329f960ab492
nanohafight <eo2gus66@gmail.com>
2023-05-17 18:05:08 +0900

    Feat : Yaml DB 로컬 저장  생성
    Yaml DB 로컬 저장  생성
    설정 데잉터 를 로컬로 저장 하여 데이터를 관리

--- Assets/3.Script/Manager/TitleUiManager.cs
+++ Assets/3.Script/Manager/TitleUiManager.cs
@@ -9,18 +9,33 @@ public class TitleUiManager : MonoBehaviour
 {
     [Header("Windows")]
     [SerializeField] GameObject settingWindow;
+    [SerializeField] GameObject resolutionWindow;
+    [SerializeField] TMP_Text resolutionCount;
 
     [Header("Setting Window")]
     [SerializeField] TMP_Dropdown resolution;
     [SerializeField] Slider slider_Bgm;
     [SerializeField] Slider slider_Sfx;
 
-    float bgmValue = 1f;
-    float sfxValue = 1f;
+    [Header("Icon Image")]
+    [SerializeField] Image image_Bgm;
+    [SerializeField] Image image_Sfx;
+    [SerializeField] Sprite[] sprite_Bgm;
+    [SerializeField] Sprite[] sprite_Sfx;
+
+    private bool isResolution = false;
+
+    private void Start()
+    {
+        slider_Bgm.value = DataManager.instance.LoadSound()[0];
+        slider_Sfx.value = DataManager.instance.LoadSound()[1];
+        resolution.value = DataManager.instance.LoadResolution();
+    }
 
     public void BGM_VolumeSetting()  // 배경음 소리 설정
     {
         AudioManager.instance.bgmPlay.volume = slider_Bgm.value;
+
     }
 
     public void SFX_VolumeSetting()  // 효과음 소리 설정
@@ -33,14 +48,15 @@ public class TitleUiManager : MonoBehaviour
 
     public void SaveButton()  // 저장 버튼 메소드
     {
-        bgmValue = slider_Bgm.value;
-        sfxValue = slider_Sfx.value;
+        DataManager.instance.SaveSound(slider_Bgm.value, slider_Sfx.value);
     }
 
     public void BackButton()  // 뒤로가기 버튼 메소드
     {
-        slider_Bgm.value = bgmValue;
-        slider_Sfx.value = sfxValue;
+        isResolution = false;
+
+        slider_Bgm.value = DataManager.instance.LoadSound()[0];
+        slider_Sfx.value = DataManager.instance.LoadSound()[1];
         settingWindow.SetActive(false);
     }
 
@@ -51,6 +67,7 @@ public class TitleUiManager : MonoBehaviour
 
     public void SettingWindow()  // 옵션 창 호출 버튼 메소드
     {
+        isResolution = true;
         settingWindow.SetActive(true);
     }
 
@@ -61,6 +78,11 @@ public class TitleUiManager : MonoBehaviour
 
     public void DropDownValue()
     {
+        if(!isResolution)
+        {
+            return;
+        }
+
         if(resolution.value == 0)
         {
             Screen.SetResolution(1920, 1080, false);
@@ -73,5 +95,48 @@ public class TitleUiManager : MonoBehaviour
         {
             Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
         }
+
+        StartCoroutine(nameof(ResolutionSetting_co));
+    }
+
+    IEnumerator ResolutionSetting_co()
+    {
+        resolutionWindow.SetActive(true);
+
+        for (int i=0; i<5; i++)
+        {
+            resolutionCount.text = $"{5 - i}";
+            yield return new WaitForSeconds(1);
+        }
+
+        int resolutionValue = DataManager.instance.LoadResolution();
+
+        resolutionWindow.SetActive(false);
+        isResolution = false;
+
+        if (resolutionValue == 0)
+        {
+            Screen.SetResolution(1920, 1080, false);
+        }
+        else if (resolutionValue == 1)
+        {
+            Screen.fullScreenMode = FullScreenMode.MaximizedWindow;
+        }
+        else if (resolutionValue == 2)
+        {
+            Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+        }
+
+        resolution.value = resolutionValue;
+
+        yield return new WaitForSeconds(0.5f);
+        isResolution = true;
+    }
+
+    public void CheckButton()
+    {
+        StopCoroutine(nameof(ResolutionSetting_co));
+        resolutionWindow.SetActive(false);
+        DataManager.instance.SaveResolution(resolution.value);
     }
 }
