bb754c131f4659cc5e0834cca496b721a2ce067e
HoneyBadger43 <129126886+HoneyBadger43@users.noreply.github.com>
2023-06-02 02:02:33 +0900

    fix playercontrol script

--- Assets/3.Script/Player/Item/Sword.cs
+++ Assets/3.Script/Player/Item/Sword.cs
@@ -4,8 +4,39 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
-    [SerializeField] private int attackDamage;
-    [SerializeField] private int attackRate;
+    public int attackDamage;
+    public float attackRate;
 
+    public BoxCollider meleeArea;
+    public TrailRenderer trailEffect;
 
+    public void Use()
+    {
+        StopCoroutine("Swing");
+        StartCoroutine("Swing");
+    }
+
+    IEnumerator Swing()
+    {
+        yield return new WaitForSeconds(0.1f);
+        meleeArea.enabled = true;
+        trailEffect.enabled = true;
+        yield return new WaitForSeconds(0.3f);
+        meleeArea.enabled = false;
+        yield return new WaitForSeconds(0.3f);
+        trailEffect.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (coll.CompareTag("Monster"))
+        {
+            Debug.Log("몬스터피격");
+            coll.TryGetComponent(out MonsterControl monsterControl);
+            int damage = PlayerControl.instance.playerData.status.attack + attackDamage;
+            Vector3 hitPoint = coll.ClosestPoint(transform.position);
+            Vector3 hitNormal = transform.position - coll.transform.position;
+            monsterControl.OnDamage(damage, hitPoint, hitNormal);
+        }
+    }
 }
