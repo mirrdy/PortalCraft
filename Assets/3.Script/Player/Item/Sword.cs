05ea8616f0a650e2e033cf6013dfaedc063a93c2
HoneyBadger43 <129126886+HoneyBadger43@users.noreply.github.com>
2023-06-02 01:43:44 +0900

    Revert "Fix playercontrol test"
    This reverts commit 5f4f485139d5261f82cec7a843f2c1aac8624e94.

--- Assets/3.Script/Player/Item/Sword.cs
+++ Assets/3.Script/Player/Item/Sword.cs
@@ -4,39 +4,8 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
-    public int attackDamage;
-    public float attackRate;
+    [SerializeField] private int attackDamage;
+    [SerializeField] private int attackRate;
 
-    public BoxCollider meleeArea;
-    public TrailRenderer trailEffect;
 
-    public void Use()
-    {
-        StopCoroutine("Swing");
-        StartCoroutine("Swing");
-    }
-
-    IEnumerator Swing()
-    {
-        yield return new WaitForSeconds(0.1f);
-        meleeArea.enabled = true;
-        trailEffect.enabled = true;
-        yield return new WaitForSeconds(0.3f);
-        meleeArea.enabled = false;
-        yield return new WaitForSeconds(0.3f);
-        trailEffect.enabled = false;
-    }
-
-    private void OnTriggerEnter(Collider coll)
-    {
-        if (coll.CompareTag("Monster"))
-        {
-            Debug.Log("몬스터피격");
-            coll.TryGetComponent(out MonsterControl monsterControl);
-            int damage = PlayerControl.instance.playerData.staters.attack + attackDamage;
-            Vector3 hitPoint = coll.ClosestPoint(transform.position);
-            Vector3 hitNormal = transform.position - coll.transform.position;
-            monsterControl.OnDamage(damage, hitPoint, hitNormal);
-        }
-    }
 }
