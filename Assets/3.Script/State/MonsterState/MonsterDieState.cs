d6f08078fbbbe85637609aca03a2b8306db70c65
peppersoltsugar <128586055+peppersoltsugar@users.noreply.github.com>
2023-06-01 22:47:56 +0900

    Fix : 몬스터 사망, 공격제한

--- Assets/3.Script/State/MonsterState/MonsterDieState.cs
+++ Assets/3.Script/State/MonsterState/MonsterDieState.cs
@@ -2,24 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DieState : EntityState
+public class MonsterDieState : EntityState
 {
     public override void EnterState(LivingEntity entity)
     {
         if (entity.onDeath != null && entity.isDead)
         {
             entity.onDeath.Invoke();
-            entity.gameObject.SetActive(false);
+            entity.StartCoroutine(Die_co(entity));
         }
     }
 
     public override void ExitState(LivingEntity entity)
     {
-        
+        entity.animator.SetBool("isDead", false);
+        entity.gameObject.SetActive(false);
     }
 
     public override void UpdateState(LivingEntity entity)
     {
-        
+            
+    }
+    private IEnumerator Die_co(LivingEntity entity)
+    {
+        entity.animator.SetBool("isDead", true);
+        yield return new WaitForSeconds(5f);
+        entity.ChangeState(new MonsterIdleState());
     }
 }
