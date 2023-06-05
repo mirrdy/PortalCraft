378bf123464aff2a32b9aecbc52db7a2e9193686
HoneyBadger43 <129126886+HoneyBadger43@users.noreply.github.com>
2023-06-06 03:18:16 +0900

    Feat : 커스터마이징 적용 && 퀵슬롯장비사용

---
+++ Assets/3.Script/Player/Item/Pickaxe.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickaxe : MonoBehaviour
+{
+    public float attackRate;
+    public int attackDamage;
+}
